4690d84d3482d361d83d1fb2e317f0afe5c65d70
GarethWoodman <gareth.woodman92@gmail.com>
2019-08-19 08:21:42 +0100

    Added possible moves to Bishop

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,3 +1,21 @@
+module Moves  
+  def cell(x, y, board, current_piece)
+    unless out_of_bounds(x, y)
+      piece = board.update[x][y]
+      if "OO" == piece
+        current_piece.moves << [x, y]
+      else 
+        (current_piece.moves << [x, y]) if @colour != piece.colour
+        return false
+      end
+    end
+  end
+
+  def out_of_bounds(x, y)
+    (x > 7 or x < 0 or y > 7 or x < 0)
+  end
+end
+
 class Pawn
   attr_reader :symbol, :colour
   attr_accessor :moves, :current_location
@@ -33,6 +51,7 @@ class Pawn
 end
  
 class Rook
+  include Moves
   attr_reader :symbol, :colour
   attr_accessor :moves, :current_location
  
@@ -45,46 +64,27 @@ class Rook
     x = @current_location[0]
     y = @current_location[1]
     @moves = Array.new
-    all_pieces = Array.new
-    board.white_pieces.each { |piece| all_pieces << piece }
-    board.black_pieces.each { |piece| all_pieces << piece }
-    check_for_pieces(all_pieces, board, x, y)
+    check_for_pieces(board, x, y)
   end
 
-  def check_for_pieces(pieces, board, x, y)
-      (1..8).each do |i|
-        break unless up(x+i, y, board)
-      end
+  def check_for_pieces(board, x, y)
+    (1..7).each do |i|
+      break unless cell(x+i, y, board, self)
+    end
 
-      (1..8).each do |i|
-        break unless up(x-i, y, board)
-      end
+    (1..7).each do |i|
+      break unless cell(x-i, y, board, self)
+    end
 
-      (1..8).each do |i|
-        break unless up(x, y+i, board)
-      end
+    (1..7).each do |i|
+      break unless cell(x, y+i, board, self)
+    end
 
-      (1..8).each do |i|
-        break unless up(x, y-i, board)
-      end
+    (1..7).each do |i|
+      break unless cell(x, y-i, board, self)
     end
   end
-
-  def up(x, y, board)
-    unless out_of_bounds(x, y)
-      piece = board.update[x][y]
-      if "OO" == piece
-        @moves << [x, y]
-      else 
-        (@moves << [x, y]) if @colour != piece.colour
-        return false
-      end
-    end
-  end
-
-  def out_of_bounds(x, y)
-    (x > 7 or x < 0 or y > 7 or x < 0)
-  end
+end
  
 class Knight
   attr_reader :symbol, :colour
@@ -96,11 +96,39 @@ class Knight
 end
  
 class Bishop
+  include Moves
+  
   attr_reader :symbol, :colour
+  attr_accessor :moves, :current_location
  
   def initialize(colour)
     @colour = colour
     @symbol = @colour[0] + "b"
+  end
+
+  def possible_moves(board)
+    x = @current_location[0]
+    y = @current_location[1]
+    @moves = Array.new
+    check_for_pieces(board, x, y)
+  end
+
+  def check_for_pieces(board, x, y)
+    (1..7).each do |i|
+      break unless cell(x+i, y+i, board, self)
+    end
+
+    (1..7).each do |i|
+      break unless cell(x-i, y-i, board, self)
+    end
+
+    (1..7).each do |i|
+      break unless cell(x+i, y-i, board, self)
+    end
+
+    (1..7).each do |i|
+      break unless cell(x-i, y+i, board, self)
+    end
   end
 end
  
